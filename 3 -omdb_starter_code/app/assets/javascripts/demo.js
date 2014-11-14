9117361cff386b07ce1f05d042118af249dd409b
Jon1213 <jon1213@gmail.com>
2014-11-14 13:51:26 -0800

    Finished the form submission

--- 3 -omdb_starter_code/app/assets/javascripts/demo.js
+++ 3 -omdb_starter_code/app/assets/javascripts/demo.js
@@ -1,28 +1,49 @@
 // get the party started
 $(document).ready(function(){
 	// hide our spinner
-
+	$(".loading").hide();
 		// remove our success logic from the ajax response - save it for $when, make sure to return the promise object
+		$(document).ready(function(){
+			// When the form is submitted...
+		    $(".movieSearch").on("submit", function(e)  {
+		        // prevent the default, show the spinner 
+		        e.preventDefault();
+		        $(".loading").show();
+		        // set the value of what the user typed in to some variable
+		        var sTerm = $(".title").val();
+		        // use $.when to make our AJAX call
+			    $.when(movieQuery(sTerm)).done(function(response){
+			    	// erase previous movie data and hide the spinner and clear any previous search values
+			    	$(".loading").hide();
+			    	$("#movieData").html("");
+			    	$(".title").val();
+			    	result = JSON.parse(response);
+			    	console.log(result.Search);
+			    	listMovies(result);
+			    });
+		    });
+		});
 
-		// When the form is submitted...
+		movieQuery = function(search){
+		    return $.ajax({
+		      url: "http://www.omdbapi.com/",
+		            data: {s: search}
+		    });
+		};
 
-		// prevent the default, show the spinner and clear any previous search values
-
-		// set the value of what the user typed in to some variable
-
-			// use $.when to make our AJAX call
-
-
-					// erase previous movie data and hide the spinner
-
-					// When we get our data back, append it to the page
-
+		listMovies = function(movies){
+		  var ul = $("<ul>").attr("id", "movies");
+		  for (var key in movies.Search) {
+		  	console.log(movies.Search[key]);
+		    var li = HandlebarsTemplates["demo/index"](movies.Search[key]);
+		    ul.append(li);
+		  }
+		  $("#movieData").append(ul);
+		};
+		
 					// BONUS 1 check to see if we have a valid search
 
 					// BONUS 2 otherwise, append our precompiled Handlebars template to a div
-
-
-			// clear the value of the text field
 
 });
 
